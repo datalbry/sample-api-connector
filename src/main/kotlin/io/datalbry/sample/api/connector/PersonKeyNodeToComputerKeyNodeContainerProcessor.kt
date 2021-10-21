cf5b734bfabc47ba8080f10457a9d073c2219613
Zeljko Bekcic <bekciczeljko@gmail.com>
2021-10-21 12:12:42 +0200

    Adding ids to document

--- src/main/kotlin/io/datalbry/sample/api/connector/PersonKeyNodeToComputerKeyNodeContainerProcessor.kt
+++ src/main/kotlin/io/datalbry/sample/api/connector/PersonKeyNodeToComputerKeyNodeContainerProcessor.kt
@@ -2,11 +2,9 @@ package io.datalbry.sample.api.connector
 
 import io.datalbry.connector.api.document.DocumentProcessor
 import io.datalbry.sample.api.client.feign.SampleApiClient
-import io.datalbry.sample.api.connector.extensions.toDocument
 import io.datalbry.sample.api.connector.extensions.toFeign
 import io.datalbry.sample.api.connector.extensions.toKeyNode
 import io.datalbry.sample.api.connector.model.ComputerKeyNodeContainer
-import io.datalbry.sample.api.connector.model.PersonDocument
 import io.datalbry.sample.api.connector.model.PersonKeyNode
 import org.springframework.stereotype.Component
 
@@ -16,7 +14,7 @@ class PersonKeyNodeToComputerKeyNodeContainerProcessor(private val client: Sampl
     override fun process(edge: PersonKeyNode): Collection<ComputerKeyNodeContainer> {
         return listOf(
             ComputerKeyNodeContainer(
-                client.getComputerIds(edge.toFeign()).asSequence().toList().map {it.toKeyNode() }
+                client.getComputerIds(edge.toFeign()).asSequence().toList().map { it.toKeyNode() }
             )
         )
     }
--- src/main/kotlin/io/datalbry/sample/api/connector/model/ProjectDocument.kt
+++ src/main/kotlin/io/datalbry/sample/api/connector/model/ProjectDocument.kt
@@ -1,13 +1,14 @@
 package io.datalbry.sample.api.connector.model
 
+import io.datalbry.connector.api.annotation.property.Id
 import io.datalbry.connector.api.annotation.stereotype.Document
 import io.datalbry.precise.api.schema.SchemaAware
 
 @SchemaAware
 @Document
 data class ProjectDocument(
-   val id: String,
-   val personId: String,
-   val name: String,
-   val projectInformation: ProjectInformationRecord
+    @Id val id: String,
+    @Id val personId: String,
+    val name: String,
+    val projectInformation: ProjectInformationRecord
 )
